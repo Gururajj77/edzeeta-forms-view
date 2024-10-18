c57e2c8a0855daeddc9c01c091d5ba744fb103c5
Gururajj77 <gururajtakeshi@gmail.com>
2024-10-18 11:37:37 +0530

    edzeeta admin final changes

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,22 @@
+// firebase.ts or firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnFHjFq7Z9amSUT5qm2DfZcusNlFK2Nic",
-  authDomain: "edzeeta-485a6.firebaseapp.com",
-  projectId: "edzeeta-485a6",
-  storageBucket: "edzeeta-485a6.appspot.com",
-  messagingSenderId: "939670710610",
-  appId: "1:939670710610:web:f22143388b03fb7183328b",
-  measurementId: "G-KZBWQ799VE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize services
 export const auth = getAuth(app);
-export const db = getFirestore(app, "forms-data");
+export const db = getFirestore(app);
